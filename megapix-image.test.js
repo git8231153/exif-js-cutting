286614d39e2040ad90abf7d3a3126dc84b3c71d2
HanShuliang <385322465@qq.com>
2015-01-15 18:17:30 +0800

    更新
    修改了与增加图片默认压缩的最大比例  裁剪比例 裁剪的大小

--- megapix-image.test.js
+++ megapix-image.test.js
@@ -1,7 +1,13 @@
-//裁剪比例
+// change图片默认压缩的最大比例  裁剪比例 裁剪的大小
+var changeWidth="640";
+var changeHeight="640";
 var minWidth="120";
 var minHeight="120";
+var cancelImagesWidth="240";
+var cancelImagesHeight="240";
+
 var Ratio="1/1"
+
 window.onload = function() {
 
 //参数调用 调用的插件 压缩后的显示对象  压缩   是否裁剪  
